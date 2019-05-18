d7209d2adf1dc52c3fb348200b9821cbb9323d10
Хвича Кардава <hvicakardava@MacBook-Air-Hvica.local>
2019-05-18 21:09:36 +0300

    изменил определение offerPromotion

--- model/sale/offerPromotion.py
+++ model/sale/offerPromotion.py
@@ -1,4 +1,10 @@
 class OfferPromotion(object):
-    special: bool
-    def __init__(self, special):
+    __special: bool
+    def __init__(self, special: bool):
         self.special = special
+    @property
+    def special(self) -> bool:
+        return self.__special
+    @special.setter
+    def special(self, value: bool):
+        self.__special = value
