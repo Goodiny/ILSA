3fdd66505427a611a04945969bd936276fc51544
Hvicha <in-fo@inbox.ru>
2019-05-19 05:22:24 +0300

    добавил абстрактный метод add

--- model/sale/saleList.py
+++ model/sale/saleList.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class SaleList(object):
     __nextPageToken: str
     def __init__(self, nextPageToken, kind='sale#saleList'):
@@ -9,3 +11,6 @@ class SaleList(object):
     @property
     def nextPageToken(self) -> str:
         return self.__nextPageToken
+    @abstractmethod
+    def add(self, obj):
+        pass
