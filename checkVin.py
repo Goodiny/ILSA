e9eebe49af5a1a2d9ca1575cca7d6c5f29cd9c43
Hvicha <in-fo@inbox.ru>
2019-06-14 00:53:12 +0300

    Update checkVin.py
    optimized checked and developed response

--- checkVin.py
+++ checkVin.py
@@ -14,14 +14,19 @@ def replace_all(txt, r_dict):
 def isValidVin(vin):
     vin = vin.upper()
     available_symbol_pattern = re.compile(r'[^A-HJ-NPR-Z0-9]')
-    check_number_pattern = re.compile(r'[^0-9X]')
-    if len(vin) < 17 or re.search(available_symbol_pattern, vin) or re.search(check_number_pattern,vin[8]) or not(vin[-4:].isdigit()):   #or find_any(vin, ['I', 'O', 'Q']) == True
+    # check_number_pattern = re.compile(r'[^0-9X]')
+    if len(vin) < 17:   #or find_any(vin, ['I', 'O', 'Q']) == True
         return -1
-    check_num = vin[8]
-    # print(check_num)
-    widths = [8,7,6,5,4,3,2,10,'-',9,8,7,6,5,4,3,2]
-    # print()
+    if re.search(available_symbol_pattern, vin) or not(vin[-4:].isdigit()):
+        return -2
+    # if re.search(check_number_pattern,vin[8]):
+    #     return -3
+    widths = [8,7,6,5,4,3,2,10,'-',9,8,7,6,5,4,3,2]    
     eq = {'A':'1','B':'2','C':'3','D':'4','E':'5','F':'6','G':'7','H':'8','J':'1','K':'2','L':'3','M':'4','N':'5','P':'7','R':'9','S':'2','T':'3','U':'4','V':'5','W':'6','X':'7','Y':'8','Z':'9'}
+    check_num = vin[8]
+    if not(check_num.isdigit()) and check_num != 'X':
+        check_num = eq[check_num]
+    print(check_num)
     new_vin = replace_all(vin, eq)
     # print(new_vin)
     count = 0
@@ -37,11 +42,16 @@ def isValidVin(vin):
 def checkVin():
     vin = input('Введите vin для проверки, для выхода введите символ(!): ')
     while vin != '!':
-        if isValidVin(vin) == 0:
+        checked = isValidVin(vin)
+        if checked == 0:
             print('Vin не является валидным так как результирующий символ не совпадает')
-        if isValidVin(vin) == -1:
+        if checked == -1:
+            print('Колисечство введенных символов не достаточно для проверик Vin')
+        if checked == -2:
             print('Vin содержаться недопустимые символы')
-        if isValidVin(vin) == 1:
+        if checked == -3:
+            print('Vin результирующий символ нельзя проверить')
+        if checked == 1:
             print('Vin является валидным')
         vin = input('Введите vin для проверки, для выхода введите символ(!): ')
 
